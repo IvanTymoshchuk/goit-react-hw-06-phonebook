088e9d3e9989f22ffe439d33ca23faffe080531f
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-06-19 14:57:38 +0200

    up

--- src/components/hooks/useLocaleStorage.jsx
+++ src/components/hooks/useLocaleStorage.jsx
@@ -1,16 +1,16 @@
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-// const useLocaleStorage = (key, defaultValue) => {
-//   // записуємо у локал сторедж те що користувач вписує , якщо нічого не має то буде значення по дефолту
-//   const [contact, setState] = useState(() => {
-//     return JSON.parse(localStorage.getItem(key) ?? defaultValue);
-//   });
+const useLocaleStorage = (key, defaultValue) => {
+  // записуємо у локал сторедж те що користувач вписує , якщо нічого не має то буде значення по дефолту
+  const [contact, setState] = useState(() => {
+    return JSON.parse(localStorage.getItem(key) ?? (defaultValue));
+  });
 
-//   useEffect(() => {
-//     // якщо користувач щось вписував то ми відображаємо йому це з локал сторедж
-//     localStorage.setItem(key, JSON.stringify(contact));
-//   }, [key, contact]);
-//   return [contact, setState];
-// }
+  useEffect(() => {
+    // якщо користувач щось вписував то ми відображаємо йому це з локал сторедж
+    localStorage.setItem(key, JSON.stringify(contact));
+  }, [key, contact]);
+  return [contact, setState];
+}
 
-// export default useLocaleStorage;  
+export default useLocaleStorage;  
